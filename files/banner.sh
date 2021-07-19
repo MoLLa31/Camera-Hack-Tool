90f7107ae7d5f9a1e8fd67138b0a1337bcf1795d
MoLLa31 <85343013+MoLLa31@users.noreply.github.com>
2021-07-19 14:29:19 +0400

    Update banner.sh

--- files/banner.sh
+++ files/banner.sh
@@ -2,29 +2,29 @@
 
 #################### GÜNLER ####################
 
-bazarertəsi=$(date |grep -o Mon)
-çərşənbəaxşamı=$(date |grep -o Tue)
-çərşənbə=$(date |grep -o Wed)
-cüməaxşamı=$(date |grep -o Thu)
-cümə=$(date |grep -o Fri)
-şənbə=$(date |grep -o Sat)
-bazar=$(date |grep -o Sun)
+pazartesi=$(date |grep -o Mon)
+sali=$(date |grep -o Tue)
+carsamba=$(date |grep -o Wed)
+persembe=$(date |grep -o Thu)
+cuma=$(date |grep -o Fri)
+cumartesi=$(date |grep -o Sat)
+pazar=$(date |grep -o Sun)
 
 #################### GÜNLERE GÖRE RENKLER ####################
 
-if [[ $bazarertəsi == Mon ]];then
+if [[ $pazartesi == Mon ]];then
 	color='\e[32m'
-elif [[ $çərşənbəaxşamı == Tue ]];then
+elif [[ $sali == Tue ]];then
 	color='\e[33m'
-elif [[ $çərşənbə == Wed ]];then
+elif [[ $carsamba == Wed ]];then
 	color='\e[34m'
-elif [[ $cüməaxşamı == Thu ]];then
+elif [[ $persembe == Thu ]];then
 	color='\e[31m'
-elif [[ $cümə == Fri ]];then
+elif [[ $cuma == Fri ]];then
 	color='\e[36m'
-elif [[ $şənbə == Sat ]];then
+elif [[ $cumartesi == Sat ]];then
 	color='\e[32m'
-elif [[ $bazar == Sun ]];then
+elif [[ $pazar == Sun ]];then
 	color='\e[33m'
 fi
 
@@ -46,18 +46,12 @@ $color
 
 
                          \e[33mSON GÜNCELLEME :\e[0m $(cat updates_infos |sed -n 1p)
-         \e[1;97m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-              $color C O D E D   B Y   M o L L a
-         \e[97m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	 \e[97m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	          $color instagram: m0ll4.tech
-  	 \e[0;97m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-		     +-+-+-+-+-+-+-+-+-+-+-+
-		         +-+-+-+-+-+-+-+
-			     +-+-+-+
-			       +-+
-			        +
+
+██████╗░██╗░░░██╗  ███╗░░░███╗░█████╗░██╗░░░░░██╗░░░░░░█████╗░
+██╔══██╗╚██╗░██╔╝  ████╗░████║██╔══██╗██║░░░░░██║░░░░░██╔══██╗
+██████╦╝░╚████╔╝░  ██╔████╔██║██║░░██║██║░░░░░██║░░░░░███████║
+██╔══██╗░░╚██╔╝░░  ██║╚██╔╝██║██║░░██║██║░░░░░██║░░░░░██╔══██║
+██████╦╝░░░██║░░░  ██║░╚═╝░██║╚█████╔╝███████╗███████╗██║░░██║
+╚═════╝░░░░╚═╝░░░  ╚═╝░░░░░╚═╝░╚════╝░╚══════╝╚══════╝╚═╝░░╚═╝
 
 "
